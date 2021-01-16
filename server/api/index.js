d314c3fe89f7188b52630ff5bef8cf3832f4eec2
SalahEddineBC <gs_bachircherif@esi.dz>
2021-01-16 22:49:11 +0100

    auth

---
+++ server/api/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable*/
+const { Router } = require('express');
+
+const router = Router();
+const authRoutes = require('./auth');
+
+
+router.use('/auth', authRoutes);
+
+module.exports = router;
