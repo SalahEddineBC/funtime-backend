8305970f3917cff92ef06aa426e8f65ee6e3b3d7
SalahEddineBC <gs_bachircherif@esi.dz>
2021-01-17 09:11:04 +0100

    user check

--- server/api/index.js
+++ server/api/index.js
@@ -3,8 +3,9 @@ const { Router } = require('express');
 
 const router = Router();
 const authRoutes = require('./auth');
-
+const userRoutes = require('./user');
 
 router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
